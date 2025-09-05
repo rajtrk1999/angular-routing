072c9d55914972958b0c0d28e7b306d4f8a7c981
rajtrk1999 <raj.trk1999@gmail.com>
2025-09-05 18:21:46 +0530

    routing

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { PlaylistComponent } from './playlist/playlist.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'Angular-Routing';
+}
